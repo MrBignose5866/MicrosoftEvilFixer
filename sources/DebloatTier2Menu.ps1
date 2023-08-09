6cf7b8e6feb2d97844db7cf599c3e981dc613f8b
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 19:33:40 -0400

    Update DebloatTier2Menu.ps1

--- sources/DebloatTier2Menu.ps1
+++ sources/DebloatTier2Menu.ps1
@@ -1,2 +1,6 @@
+# Clears Terminal Before Debloat Tier 1 Menu Launches
 Clear-Host
-Write-Output "Test"
+
+Write-Output "================================================="
+Write-Output "Debloat Tier 2: Minimal Programs (The Tier I Use)"
+Write-Output "================================================="
