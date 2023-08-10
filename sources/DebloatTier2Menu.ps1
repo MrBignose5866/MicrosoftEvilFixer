65c75b2b0247f566374b11519089feaefc1a2c5b
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 20:00:07 -0400

    Update DebloatTier2Menu.ps1

--- sources/DebloatTier2Menu.ps1
+++ sources/DebloatTier2Menu.ps1
@@ -1,6 +1,19 @@
-# Clears Terminal Before Debloat Tier 1 Menu Launches
+# Clears Terminal Before Debloat Tier 2 Menu Launches
 Clear-Host
 
 Write-Output "================================================="
 Write-Output "Debloat Tier 2: Minimal Programs (The Tier I Use)"
 Write-Output "================================================="
+
+Write-Output "======================="
+Write-Output "Apps Kept After Debloat"
+Write-Output "======================="
+
+Write-Output "File Explorer, Settings, and System Stuff of Course"
+Write-Output "Calculator"
+Write-Output "Snipping Tool"
+Write-Output "Paint"
+Write-Output "Windows Terminal"
+Write-Output "Notepad"
+Write-Output "Microsoft Edge (This Can't Be Removed Easily)"
+Write-Output "Microsoft Store (This Can't Be Removed Easily)"
