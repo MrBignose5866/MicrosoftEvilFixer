d942c9353cc78a45e198a4412b5914fdf8d45633
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 20:04:15 -0400

    Update DebloatTier1Menu.ps1

--- sources/DebloatTier1Menu.ps1
+++ sources/DebloatTier1Menu.ps1
@@ -4,3 +4,13 @@ Clear-Host
 Write-Output "==========================================="
 Write-Output "Debloat Tier 1: No Bloat (Super Clean Tier)"
 Write-Output "==========================================="
+
+Write-Output "======================="
+Write-Output "Apps Kept After Debloat"
+Write-Output "======================="
+
+Write-Output "File Explorer, Settings, and System Stuff of Course"
+Write-Output "Windows Terminal"
+Write-Output "Notepad"
+Write-Output "Microsoft Edge (This Can't Be Removed Easily)"
+Write-Output "Microsoft Store (This Can't Be Removed Easily)"
