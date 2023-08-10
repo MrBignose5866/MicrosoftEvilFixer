0780852e5cbc6fd47513e6584a1242b16fb41f34
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 20:15:55 -0400

    Update DebloatTier3Menu.ps1

--- sources/DebloatTier3Menu.ps1
+++ sources/DebloatTier3Menu.ps1
@@ -1,2 +1,20 @@
+# Clears Terminal Before Debloat Tier 2 Menu Launches
 Clear-Host
-Write-Output "Test"
+
+Write-Output "================================================="
+Write-Output "Debloat Tier 3:   Almost Stock    (Consumer Tier)"
+Write-Output "================================================="
+
+Write-Output "======================="
+Write-Output "Apps Kept After Debloat"
+Write-Output "======================="
+
+Write-Output "File Explorer, Settings, and System Stuff of Course"
+Write-Output "Calculator"
+Write-Output "Alarms and Clock"
+Write-Output "Snipping Tool"
+Write-Output "Paint"
+Write-Output "Windows Terminal"
+Write-Output "Notepad"
+Write-Output "Microsoft Edge (This Can't Be Removed Easily)"
+Write-Output "Microsoft Store (This Can't Be Removed Easily)"
