744e81b448c4beeefe1ce5869ce9122569642c03
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 20:44:19 -0400

    Update DebloatTier1Menu.ps1

--- sources/DebloatTier1Menu.ps1
+++ sources/DebloatTier1Menu.ps1
@@ -14,3 +14,10 @@ Write-Output "Windows Terminal"
 Write-Output "Notepad"
 Write-Output "Microsoft Edge (This Can't Be Removed Easily)"
 Write-Output "Microsoft Store (This Can't Be Removed Easily)"
+
+$userinput = Read-Host -Prompt "Press A To Start Script"
+
+if ($userinput -eq 'A'){
+    Clear-Host
+    & ".\DebloatTier1.ps1"
+}
