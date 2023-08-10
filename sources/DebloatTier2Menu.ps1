c89413e9fa96f2f486df41cfa01349d918d8a6da
ThatPowerShellGuy <85959880+ThatPowerShellGuy1@users.noreply.github.com>
2023-08-09 21:09:48 -0400

    Update DebloatTier2Menu.ps1

--- sources/DebloatTier2Menu.ps1
+++ sources/DebloatTier2Menu.ps1
@@ -5,6 +5,9 @@ Write-Output "================================================="
 Write-Output "Debloat Tier 2: Minimal Programs (The Tier I Use)"
 Write-Output "================================================="
 
+# Leave A Space
+Write-Output ""
+
 Write-Output "======================="
 Write-Output "Apps Kept After Debloat"
 Write-Output "======================="
@@ -17,6 +20,9 @@ Write-Output "Windows Terminal"
 Write-Output "Notepad"
 Write-Output "Microsoft Edge (This Can't Be Removed Easily)"
 Write-Output "Microsoft Store (This Can't Be Removed Easily)"
+
+# Leave A Space
+Write-Output ""
 
 $userinput = Read-Host -Prompt "Press A To Start Script"
 
